906dddb8446cf3d2940a2cf5ebf06bcd45680b4f
aricWL <aricliesenfelt@gmail.com>
2017-02-12 00:54:06 -0800

    updating

--- app.py
+++ app.py
@@ -10,8 +10,22 @@ def root():
 def index():
 	return render_template('index.html')
 
-@app.route('/representative/<name>')
+@app.route('/representative', methods=["GET", "POST"])
 def representative():
+
+	print("representation page")
+	if request.method == "POST":
+		return render_template('rep.html', first=request.form['first'], last=request.form['last'], city=request.form['city'])
+
+	else:
+		pass
+
+@app.route('/participant', methods=["POST"])
+def participant():
+	print("participation page")
+
+@app.route('/<lastname>')
+def rep():
 	pass
 
     
