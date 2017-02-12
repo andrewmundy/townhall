364e2bedb251477e13f86df78d15a77e0bf644c0
aricWL <aricliesenfelt@gmail.com>
2017-02-11 22:40:15 -0800

    updating app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, url_for, redirect, request, flash
-from flask_modus import Modus
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root():
+	return redirect(url_for('index'))
+
+@app.route('/home')
+def index():
+	return render_template('index.html')
+
+@app.route('/representative/<name>')
+def representative():
+	pass
+
     
 if __name__ == "__main__":
     app.run(port=4001, debug=True)
